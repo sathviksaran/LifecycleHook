6db52d6ba67750ec02f6c6568c404144059cf207
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2024-05-03 23:20:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DemoComponent } from "./demo/demo.component";
+import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, DemoComponent, NgIf]
 })
 export class AppComponent {
   title = 'LifecycleHook';
+
+  inputText: string = '';
+  destroy: boolean = true;
+  
+  onSubmit(inputEl: HTMLInputElement){
+    this.inputText = inputEl.value;
+  }
+
+  DestroyComponent(){
+    this.destroy = false;
+  }
 }
